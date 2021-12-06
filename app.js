ae324d38552f08a3094cdff01421b65090a7e844
varis1993 <91279856+varis1993@users.noreply.github.com>
2021-12-06 12:04:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ console.log(profileDataArgs);
 
 
 
-const printProfileData = (profileDataArr) => {
-  console.log(profileDataArr);
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+  for (let i = 0; i < profileDataArr.length; i++) {
+    console.log(profileDataArr[i]);
+  }
 };
-
-printProfileData(profileDataArgs);
