e8259cbb6085e23a416d50029612e3006fa8a56f
varis1993 <91279856+varis1993@users.noreply.github.com>
2021-12-06 12:01:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const printProfileData = (profileDataArr) => {
+  console.log(profileDataArr);
+};
+
+printProfileData(profileDataArgs);
