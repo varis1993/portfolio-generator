2fb3b877862953522c6b88cadafd5f16e11db263
varis1993 <91279856+varis1993@users.noreply.github.com>
2021-12-08 11:28:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -20,5 +20,10 @@ const generatePage = (name, github) => {
   </html>
   `;
 };
-console.log(name, github);
-console.log(generatePage(name, github));
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
