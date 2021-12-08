5608917cebcff4b60626fc117ed6ce90b43d96f5
varis1993 <91279856+varis1993@users.noreply.github.com>
2021-12-08 11:07:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-const profileDataArgs = process.argv.slice(2);
-
+const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+console.log(generatePage('Jane', 'janehub'))
