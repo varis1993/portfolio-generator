43f93b4151200c2d8280eaba9e2a2174a19bd7b1
varis1993 <91279856+varis1993@users.noreply.github.com>
2021-12-08 11:37:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
 
@@ -22,9 +23,8 @@ const generatePage = (name, github) => {
   `;
 };
 
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
